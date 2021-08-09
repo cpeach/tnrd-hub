955f222f005e434ac0536b07fbbe72a968e47f84
Cory Peach <cpeach@tnrd.ca>
2021-08-09 18:57:41 +0000

    Added rewrites

--- next.config.js
+++ next.config.js
@@ -3,4 +3,21 @@ module.exports = {
   images: {
     domains: ['tnrd-assets.s3.ca-central-1.amazonaws.com'],
   },
+  async rewrites() {
+    return [
+      {
+        source: '/:path*',
+        destination: `/:path*`,
+      },
+      {
+        source: '/api-console',
+        destination: `localhost:3001/api-console`,
+      },
+      {
+        source: '/api-console/:path*',
+        destination: `localhost:3001/api-console:path*`,
+      },
+    ]
+  },
+
 }
