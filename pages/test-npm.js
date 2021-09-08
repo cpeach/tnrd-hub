7288a51cd8c86bfbee929688e54ff399a2765a71
Jesse Phillips <jphillips@tnrd.ca>
2021-09-08 17:32:30 +0000

    test commit

--- pages/test-npm.js
+++ pages/test-npm.js
@@ -13,7 +13,6 @@ export default function TestNPM(props) {
 		
 			<div style={{"display":"inline-block","margin-top":"120px"}}>
 				<Button label="test" kind="primary"/>
-				<Button label="test" kind="primary"/>
         
 			</div>
 	)
