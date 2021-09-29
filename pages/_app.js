91f668746cce10e5062d54cb0546e855091ed09c
Cory Peach <cpeach@tnrd.ca>
2021-09-29 21:41:54 +0000

    Added a validator to check if userand token exist in local data

--- pages/_app.js
+++ pages/_app.js
@@ -15,8 +15,17 @@ export default function Hub({ Component, pageProps }) {
     var ref   = localStorage.getItem('user');
     var _user = await client({url:"/api-console/users/"+ref});
     var _apps = await client({url:"/api-console/applications"});
-    if(_user.length<1&&router.pathname!=='/signin'){router.push('/signin')}
+    if((_user.length<1&&router.pathname.indexOf('/signin')===-1)&&ref){
+      router.push('/signin')
+    }
     if(isMounted){
+      setInterval(async() => {
+          if(!(localStorage.getItem('user')&&localStorage.getItem('token'))){
+             router.push('/signin')
+          }else{
+               var session = await client({url:"/api-console/validate"});
+          }
+      }, 10000);
       setUser(_user);
       setApps(_apps);
     }
