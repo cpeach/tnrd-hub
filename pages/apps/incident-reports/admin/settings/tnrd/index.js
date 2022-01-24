004a50dd4b623d63f0422f432a54669c5f1a0a62
Cory Peach <cpeach@tnrd.ca>
2022-01-24 23:17:03 +0000

    Update incident report items

--- pages/apps/incident-reports/admin/settings/tnrd/index.js
+++ pages/apps/incident-reports/admin/settings/tnrd/index.js
@@ -46,16 +46,16 @@ export default function Update(props){
 
 		
 		form.fields[0].options = administrators.map(item=>({label:item.name,name:item.short,value:item._id}));
-		form.fields[0].value = settings.["recipients-cupe"];
+		form.fields[0].value = settings["recipients-cupe"];
 
 		form.fields[1].options = administrators.map(item=>({label:item.name,name:item.short,value:item._id}));
-		form.fields[1].value = settings.["recipients-bcgeu"];
+		form.fields[1].value = settings["recipients-bcgeu"];
 
-		form.fields[2].value = settings.["general-types"];
-		form.fields[3].value = settings.["critical-types"];
+		form.fields[2].value = settings["general-types"];
+		form.fields[3].value = settings["critical-types"];
 
-		form.fields[4].value = settings.["departments"];
-		form.fields[5].value = settings.["locations"];
+		form.fields[4].value = settings["departments"];
+		form.fields[5].value = settings["locations"];
 		
 
 		return <Form user={props.user} apps={props.apps} data={form} active="1" onSubmit={handleSubmit}  />
